ca4a2ebebfd3a7f41fe7362a3a3006195ec21285
gustavo.segantini <rm560111@fiap.com.br>
2025-12-19 19:14:08 -0300

    refactor: remove unused imports from CnabApiFactory
    Clean up unused 'using Microsoft.Extensions.Caching.Memory' import

--- backend.IntegrationTests/CnabApiFactory.cs
+++ backend.IntegrationTests/CnabApiFactory.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Caching.Memory;
 using CnabApi.Data;
 
 namespace CnabApi.IntegrationTests;

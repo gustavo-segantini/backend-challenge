ca639fcdc2a7cb466778de498a4f8a5e85e72589
gustavo.segantini <rm560111@fiap.com.br>
2025-12-19 19:13:47 -0300

    refactor: remove unused imports from DataSeeder
    Clean up unused imports:
    - 'using Microsoft.Extensions.DependencyInjection'
    - 'using Microsoft.Extensions.Logging'

--- backend/Data/Seed/DataSeeder.cs
+++ backend/Data/Seed/DataSeeder.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using CnabApi.Models;
 
 namespace CnabApi.Data.Seed;
